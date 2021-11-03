860d073a0b6ed47262e04cfeac0472148f8a9a55
jotades13 <jotahemmydesenvolvedor.email.com>
2021-11-03 07:35:21 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+
 function App() {
   return (
-    <div className="text-primary">Olá Jotehemmy, começando com React!
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Jotehemmy, com React!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
